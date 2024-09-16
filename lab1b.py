0d1989be13027a2d8afa9af13267be05699e38d3
SHAMS BIN HARUN <sbharun@myseneca.ca>
2024-09-16 15:45:03 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: This program asks how old Isaac is.'''
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+
+# Create a string object
+name = 'Isaac'
+
+# Print the question to the screen
+print('How old are you ' + name + '?')
