07c117db84e2e80bd776a8b17b21652e7e7ea1b9
SHAMS BIN HARUN <sbharun@myseneca.ca>
2024-09-16 15:54:00 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates math operations in Python.'''
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+# Create objects for x, y, and z
+x = 10
+y = 2
+z = 5
+
+# Perform a mathematical operation and print the result
+print(f'{x} + {y} * {z} = {x + y * z}')
